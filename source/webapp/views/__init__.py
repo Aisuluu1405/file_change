ff7afa4348cc047a13d95b931520cfedaaa3f241
Айсулу <aisuluueco2009@yandex.ru>
2020-03-14 20:02:06 +0600

    пыталась сделать 5 этап

--- source/webapp/views/__init__.py
+++ source/webapp/views/__init__.py
@@ -1,2 +1,1 @@
 from webapp.views.files_views import IndexView, FileCreateView, FileDetailView, FileUpdateView, FileDeleteView
-# from webapp.views.private_views import SearchUserView
--- source/webapp/views/private_views.py
+++ source/webapp/views/private_views.py
@@ -1,13 +1,16 @@
-# pk объекта Favorite теперь в ответе не нужен, т.к.
-# соответствующий DeleteFromFavorites на него больше не
-# ориентируется.
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views import View
+from django.views.generic import TemplateView
+
 from webapp.models import Private, File
 
+
 class AddToPrivates(LoginRequiredMixin, View):
+
     def post(self, request, *args, **kwargs):
         user = request.user
         file = get_object_or_404(File, pk=request.POST.get('pk'))
@@ -23,3 +26,21 @@ class DeleteFromPrivates(LoginRequiredMixin, View):
         file = get_object_or_404(File, pk=request.POST.get('pk'))
         Private.objects.filter(file=file, user=user).delete()
         return JsonResponse({'pk': file.pk})
+
+#
+# class SearchUser(TemplateView):
+#     template_name = "user-search.html"
+#
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         result_list = []
+#         q = self.request.GET.get('q', None)
+#         if q:
+#             result_list = [user for user in User.objects.filter(
+#                 Q(username__icontains=q))]
+#             context = {
+#                 "result": result_list,
+#                 "query": q,
+#             }
+#         return context
